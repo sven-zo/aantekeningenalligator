f11cfacafcbd8044af6cc0e138738597d357950f
sven-zo <svenzo@outlook.com>
2017-09-11 20:10:04 +0200

    feat(aa): adds instant-loading and a course link

---
+++ assets/js/init.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+  Barba.Pjax.Dom.wrapperId = 'aa-static';
+  Barba.Pjax.Dom.containerClass = 'aa-dynamic';
+  Barba.Pjax.start();
+  console.log('AantekeningenAlligator - Inititalised');
+})
