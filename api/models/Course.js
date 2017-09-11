eb284f32e375d716035f6189cf61ac540cc6cd86
sven-zo <svenzo@outlook.com>
2017-09-11 15:16:13 +0200

    feat(aa): Added Course model and controller

---
+++ api/models/Course.js
@@ -0,0 +1,17 @@
+/**
+ * Course.js
+ *
+ * @description :: Represents a course.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string',
+      required: true
+    }
+  }
+};
+
