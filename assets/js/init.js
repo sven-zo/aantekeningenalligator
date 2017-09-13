90ed7cbd1955793cbc075b54932dca732957c2ed
sven-zo <svenzo@outlook.com>
2017-09-13 13:51:34 +0200

    fix(aa): turns off barba, and turns off layout

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', () => {
-  Barba.Pjax.Dom.wrapperId = 'aa-static';
-  Barba.Pjax.Dom.containerClass = 'aa-dynamic';
-  Barba.Pjax.start();
+  // TODO: fix this or remove it
+  // Barba.Pjax.Dom.wrapperId = 'aa-static';
+  // Barba.Pjax.Dom.containerClass = 'aa-dynamic';
+  // Barba.Pjax.start();
   console.log('AantekeningenAlligator - Inititalised');
 })
