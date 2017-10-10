7044aaaa5875a34e61b9e3da0e393dab9d1ef50a
sven-zo <svenzo@outlook.com>
2017-10-10 17:37:17 +0200

    feat(aa): leaner auth controller
     * uses new auth services

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -5,30 +5,27 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-const SECRET = require('../../config/env/secret');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
 module.exports = {
   authenticate: async function (req, res) {
-    const errorView = () => res.view('auth/login', {success: false, message: 'Username or password incorrect'});
+    const username = req.body.name;
+    const password = req.body.password;
     try {
-      const username = req.body.name;
-      const password = req.body.password;
-      const user = await User.findOne({name: username});
-      if (!user) return errorView();
-      else {
-        const dbpassword = user.password;
-        const result = await bcrypt.compare(password, dbpassword);
-        if (!result) return errorView();
-        else {
-          const token = jwt.sign({user: username}, SECRET, {expiresIn: '1 days'});
-          res.cookie('AantekeningenAlligator_e4RYHTNIe3wG5PohI7xq', token, {httpOnly: true});
-          return res.view('auth/loginSucces');
-        }
-      }
+      const passwordCheck = await AuthService.checkPasswordOfUser({
+        username, password
+      });
+      const token = await AuthService.signToken({
+        username
+      });
+      res.cookie('AantekeningenAlligator_e4RYHTNIe3wG5PohI7xq', token.token, {httpOnly: true});
+      req.session.authenticated = true;
+      return res.view('auth/loginSucces');
     } catch (err) {
-      return res.negotiate(err);
+      sails.log.error(new Error(err));
+      if (err.server) {
+        return res.serverError(err.server);
+      } else {
+        return res.view('auth/login', {success: false, message: 'Username or password incorrect'});
+      }
     }
   }
 };
