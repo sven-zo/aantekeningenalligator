6c0fb3e937fe9e5a687907df191fca9e2dd0f5dc
sven-zo <svenzo@outlook.com>
2017-10-23 12:33:10 +0300

    feat(aa): adds edit & delete to coursecontroller

--- api/controllers/CourseController.js
+++ api/controllers/CourseController.js
@@ -7,6 +7,7 @@
 
 module.exports = {
   index: async function(req, res) {
+    req.session.redirect = sails.getUrlFor('Course.index');
     try {
       const courses = await Course.find().populate('category');
       let snackbar = false;
@@ -20,7 +21,7 @@ module.exports = {
     }
   },
   add: async function(req, res) {
-    // TODO: split these in the route
+    // TODO: split these in the route if possible
     if (req.method === 'POST') {
       try {
         const category = await Category.findOne({
@@ -50,10 +51,44 @@ module.exports = {
       const course = await Course.findOne({
         code: req.params.code
       }).populate('notes');
-      //return res.json({course})
       return res.view('course/courseDetail', { course });
     } catch (err) {
       return res.serverError(err);
+    }
+  },
+  // TODO: validate everything (kon via sails?)
+  edit: async function(req, res) {
+    if (req.method === 'POST') {
+      const category = await Category.findOne({
+        name: req.body.category
+      });
+      await Course.update({ code: req.params.code },
+      {
+        name: req.body.name,
+        code: req.body.code,
+        category: category.id,
+        period: req.body.period,
+        year: req.body.year
+      });
+      return res.json({placeholder: 'Updated course!'});
+    } else {
+      try {
+        const course = await Course.findOne({
+          code: req.params.code
+        });
+        //TODO: make this page
+        return res.view('course/courseEdit', { course });
+      } catch (err) {
+        return res.negotiate(err);
+      }
+    }
+  },
+  delete: async function(req, res) {
+    try {
+      await Course.destroy({ code: req.params.code });
+      return res.json({placeholder: 'Deleted course!'});
+    } catch(err) {
+      return res.negotiate(err);
     }
   }
 };
