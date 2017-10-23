2e65a2b0eb01ab9cfe38ff8680bc300765683e3d
sven-zo <svenzo@outlook.com>
2017-10-23 12:32:44 +0300

    feat(aa): adds edit & delete to categorycontroller

--- api/controllers/CategoryController.js
+++ api/controllers/CategoryController.js
@@ -6,11 +6,43 @@
  */
 
 module.exports = {
-	index: (req, res) => {
-    Category.find()
-      .exec((err, categories) => {
-        return res.view('category/categoryList', { categories });
-    });
+  index: async function (req, res) {
+    try {
+      const categories = await Category.find();
+      return res.view('category/categoryList', { categories });
+    } catch (err) {
+      return res.negotiate(err);
+    }
   },
+  // TODO: validate everything (kon via sails?)
+  // TODO: make images uploadable
+  edit: async function(req, res) {
+    if (req.method === 'POST') {
+      await Category.update({ name: req.params.category },
+      {
+        name: req.body.name,
+        image: req.body.image,
+      });
+      return res.json({placeholder: 'Updated category!'});
+    } else {
+      try {
+        const category = await Category.findOne({
+          name: req.params.category
+        });
+        //TODO: make this page
+        return res.view('category/categoryEdit', { category });
+      } catch (err) {
+        return res.negotiate(err);
+      }
+    }
+  },
+  delete: async function(req, res) {
+    try {
+      await Category.destroy({ name: req.params.category });
+      return res.json({placeholder: 'Deleted category!'});
+    } catch(err) {
+      return res.negotiate(err);
+    }
+  }
 };
 
